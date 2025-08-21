cbdfe14d51c8aff5e1c489b3c65c394ceb365243
Juan José González <jjgonzalezc@javerianacali.edu.co>
2025-08-21 17:03:38 -0500

    Primer commit Calculadora

---
+++ matematicas.cpp
@@ -0,0 +1,15 @@
+//
+// Created by juanj on 19/08/2025.
+//
+
+#include "matematicas.h"
+
+//Función de suma
+int sumar(int a, int b) {
+    return a+b;
+}
+
+//Función de resta
+int restar(int a, int b) {
+    return a-b;
+}
---
+++ matematicas.h
@@ -0,0 +1,11 @@
+//
+// Created by juanj on 19/08/2025.
+//
+
+#ifndef CALCULADORA_MATEMATICAS_H
+#define CALCULADORA_MATEMATICAS_H
+
+int sumar(int a, int b);
+int restar(int a, int b);
+
+#endif //CALCULADORA_MATEMATICAS_H
